5351828f7bbd7f651e63a2b866c6c6daa7bdd3e4
MedNewton <med.newton.2013@gmail.com>
2024-11-29 06:42:59 +0100

    Indices slider

---
+++ src/app/components/IndicesSlider.tsx
@@ -0,0 +1,124 @@
+'use client'
+import { Stack, Button } from "@mui/material";
+import theme from "@theme/theme";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { type ArrowProps } from 'react-multi-carousel/lib/types';
+import ChartCard from "./ChartCard";
+
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+import { type Index, IndicesWithDetails } from "../Indices";
+
+
+const CustomLeftArrow = ({ onClick }: ArrowProps) => {
+    return (
+        <Stack alignItems={'center'} justifyContent={'center'} position={'absolute'} left={0} marginX={{ xs: 0, lg: 2 }} sx={{
+            scale: { xs: '0.6', lg: '0.8' },
+            top: { xs: '35%', lg: '45%' },
+        }}>
+            <Button onClick={onClick} sx={{
+                backgroundColor: theme.palette.NexOpaqueDot.main,
+                paddingX: '0.1rem',
+                borderRadius: '50%',
+                aspectRatio: 1
+            }}>
+                <ArrowBackIosIcon fontSize="medium" color="info" sx={{
+                    marginLeft: 1
+                }} />
+            </Button>
+        </Stack>
+    );
+};
+
+const CustomRightArrow = ({ onClick }: ArrowProps) => {
+    return (
+        <Stack alignItems={'center'} justifyContent={'center'} position={'absolute'} right={0} marginX={{ xs: 0, lg: 2 }} sx={{
+            scale: { xs: '0.6', lg: '0.8' },
+            top: { xs: '35%', lg: '45%' },
+        }}>
+            <Button onClick={onClick} sx={{
+                backgroundColor: theme.palette.NexCardGradient2_1.main,
+                paddingX: '0.1rem',
+                borderRadius: '50%',
+                aspectRatio: 1
+            }}>
+                <ArrowForwardIosIcon fontSize="medium" color="info" />
+            </Button>
+        </Stack>
+    );
+};
+
+const IndicesSlider = () => {
+
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 1,
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
+    return (
+        <Stack
+            display={{ xs: 'none', lg: 'flex' }}
+            width={'100%'}
+            height={'100%'}
+            alignItems={'center'}
+            gap={{ xs: 3, lg: 4 }}
+            paddingY={{ xs: 2, lg: 0 }}
+            sx={{
+                backgroundColor: 'transparent',
+                '& .carousel-container': {
+                    width: '100%',
+                    height: '100%',
+                    paddingX: { xs: 0, lg: '20px 0' },
+                    borderRadius: '1.6rem',
+                },
+                '& .react-multi-carousel-track': {
+                    height: '100%'
+                },
+                '& .carousel-item': {
+                    padding: { xs: '0 0px', lg: '0 0px' },
+                    height: '100%',
+                }
+            }}
+        >
+            <Carousel
+                responsive={responsive}
+                customLeftArrow={<CustomLeftArrow />}
+                customRightArrow={<CustomRightArrow />}
+                infinite={true}
+                containerClass="carousel-container"
+                itemClass="carousel-item"
+            >
+                {IndicesWithDetails.map((item: Index, key) => (
+                    <Stack
+                        key={key}
+                        height='100%'
+                        width="100%"
+                        alignItems={'center'}
+                        gap={2}
+                        marginX={0}
+                    >
+                        <ChartCard product={item} />
+                    </Stack>
+                ))}
+            </Carousel>
+        </Stack>
+    )
+}
+
+export default IndicesSlider
--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -1,29 +1,59 @@
-import { Stack, Box } from "@mui/material";
+import { Stack, Box, Button } from "@mui/material";
 import Grid from '@mui/material/Grid2';
 import Navbar from "@sections/Navbar";
 import HeroShadows from "@components/HeroShadows";
 import HeroCard from "@components/HeroCard";
 import ChartCard from "@components/ChartCard";
 import MobileIndicesSlider from "@components/MobileIndicesSlider";
-import { IndicesWithDetails } from "../Indices";
+import IndicesSlider from "@components/IndicesSlider";
+import theme from "@theme/theme";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { type ArrowProps } from 'react-multi-carousel/lib/types';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+
+import { type Index, IndicesWithDetails } from "../Indices";
+
+
+
+
 
 const HeroSection = () => {
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 4,
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
     return (
-        <Box width={'100%'} height={{xs: 'auto',lg: '100vh'}} position={'relative'}>
+        <Box width={'100%'} height={{ xs: 'auto', lg: '100vh' }} position={'relative'}>
             <HeroShadows />
             <Navbar />
-            <Stack width={'100%'} height={{xs: '100%', lg: '88%'}} position={'relative'} zIndex={3} marginX={'auto'} marginTop={{xs: 4, lg: 2}} padding={{xs: 0, lg: 3}} direction={{xs: 'column', lg: 'row'}} alignItems={'center'} gap={{xs: 1, lg: 2}}>
-                <Box display={'flex'} width={{xs: '100%', lg: '35%'}} height={{xs: '70vh', lg: '100%'}}>
+            <Stack width={'100%'} height={{ xs: '100%', lg: '88%' }} position={'relative'} zIndex={3} marginX={'auto'} marginTop={{ xs: 4, lg: 2 }} padding={{ xs: 0, lg: 3 }} direction={{ xs: 'column', lg: 'row' }} alignItems={'stretch'} gap={{ xs: 1, lg: 2 }}>
+                <Box display={'flex'} width={{ xs: '100%', lg: '35%' }} height={{ xs: '70vh', lg: '100%' }}>
                     <HeroCard />
                 </Box>
-                <Grid display={{xs: 'none', lg: 'flex'}} flexGrow={'1'} container rowSpacing={2} columnSpacing={2} width={{xs: '100%', lg: '65%'}} height={'100%'}>
-                    {IndicesWithDetails.map((item, index) => (
-                        <Grid size={{xs: 12, lg: 6}} key={index}>
-                            <Box display={'flex'} width={'100%'} height={'100%'}>
-                                <ChartCard product={item} />
-                            </Box>
-                        </Grid>
-                    ))}
+                <Grid display={{ xs: 'none', lg: 'flex' }} flexGrow={'1'} container rowSpacing={2} columnSpacing={2} width={{ xs: '100%', lg: '65%' }} sx={{ flexGrow: 1 }}>
+                    <Box sx={{
+                        height: '100%',
+                        width: '100%',
+                    }}>
+                        <IndicesSlider />
+                    </Box>
                 </Grid>
                 <MobileIndicesSlider />
 
